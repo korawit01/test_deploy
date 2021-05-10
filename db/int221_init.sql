6ac6eaabbf1ea0633adf2fbce3f3f61e6cb8a98f
Korawit Rittisit <korawit.oum@mail.kmutt.ac.th>
2021-05-11 04:28:39 +0700

    fix code

--- db/int221_init.sql
+++ db/int221_init.sql
@@ -15,9 +15,9 @@ USE `mydb` ;
 -- -----------------------------------------------------
 -- Table `mydb`.`Brand`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`brand` ;
+DROP TABLE IF EXISTS `mydb`.`Brand` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`brand` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Brand` (
   `Brand_Id` INT NOT NULL,
   `Brand_name` VARCHAR(1000) NOT NULL,
   PRIMARY KEY (`Brand_Id`))
@@ -27,9 +27,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Product`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`product` ;
+DROP TABLE IF EXISTS `mydb`.`Product` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`product` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Product` (
   `Product_Id` INT NOT NULL AUTO_INCREMENT,
   `Product_Name` VARCHAR(1000) NOT NULL,
   `Description` VARCHAR(1000) NOT NULL,
@@ -50,9 +50,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Color`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`color` ;
+DROP TABLE IF EXISTS `mydb`.`Color` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`color` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Color` (
   `Color_Id` INT NOT NULL,
   `Color_Name` VARCHAR(1000) NOT NULL,
   PRIMARY KEY (`Color_Id`))
@@ -62,9 +62,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Have`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`have` ;
+DROP TABLE IF EXISTS `mydb`.`Have` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`have` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Have` (
   `Product_Product_Id` INT NOT NULL,
   `Color_Color_Id` INT NOT NULL,
   PRIMARY KEY (`Product_Product_Id`, `Color_Color_Id`),
